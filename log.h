b5ee3f7f21a48beec0d065d68a45d40626695e8c
GlizzyHacker <saxattila@gmail.com>
2025-05-14 14:51:20 +0200

    added logging helpers

---
+++ log.h
@@ -0,0 +1,19 @@
+﻿#include <ostream>
+#include <sstream>
+#include <cstring>
+#include <string>
+
+//KIIRÁSOKHOZ SEGÉDFÜGGVÉNYEK
+
+std::ostream& LOG = std::cout;
+
+//Egy pointerből csinál egy könnyeben olvasható azonosítót
+template<typename T>
+std::string ID(T* ptr) {
+	std::stringstream sstream;
+	sstream << ptr;
+	std::string str = sstream.str();
+	std::stringstream sstream2;
+	sstream2 << '<' << typeid(T).name() << (char)str[strlen(str.c_str()) - 1] << (char)str[strlen(str.c_str()) - 3] << (char)str[strlen(str.c_str()) - 5] << '>';
+	return sstream2.str();
+}
