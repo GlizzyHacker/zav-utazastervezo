55633fb76be622e3cba8b2cffa41272ced84d55b
GlizzyHacker <saxattila@gmail.com>
2025-05-18 14:48:50 +0200

    sorted list

---
+++ sorted_list.hpp
@@ -0,0 +1,135 @@
+#include <stdexcept>
+#include "memtrace.h"
+
+#ifndef SORTEDLIST
+#define SORTEDLIST
+
+template<typename T>
+class isLessThan {
+public:
+	bool operator()(T t1, T t2) { return t1 < t2; }
+};
+
+template<typename T, typename compare = isLessThan<T>>
+class SortedList {
+
+	class ListMember {
+	public:
+		T element;
+		
+		ListMember* prev;
+		
+		ListMember* next;
+
+		ListMember(T element, ListMember* prevP, ListMember* nextP) : element(element), prev(prevP), next(nextP) {
+			if (prev != NULL) {
+				prev->next = this;
+			}
+			if (next != NULL) {
+				next->prev = this;
+			}
+		}
+	};
+	
+	compare pred;
+
+	ListMember* list;
+	
+	size_t length;
+public:
+
+	class Iterator {
+		ListMember* member;
+	public:
+		Iterator(ListMember* member) : member(member) {}
+
+		Iterator& operator++(int i) {
+			member = member->next;
+			return *this;
+		}
+
+		Iterator& operator--(int i) {
+			member = member->prev;
+			return *this;
+		}
+
+		T operator*() const {
+			if (member == NULL) {
+				throw std::out_of_range("end of list");
+			}
+			return member->element;
+		}
+
+		bool operator ==(const Iterator& iter) const {
+			return member == iter.member;
+		}
+
+		bool operator !=(const Iterator& iter) const {
+			return !operator==(iter);
+		}
+	};
+
+	SortedList() : length(0), list(NULL) {}
+
+	size_t getLength() const {
+		return length;
+	}
+
+	void remove(T element) {
+		ListMember* ptr = list;
+		while (ptr != NULL) {
+			if (ptr->element == element) {
+				ptr->prev->next = ptr->next;
+				delete ptr;
+				length--;
+				return;
+			}
+			ptr = ptr->next;
+		}
+	}
+
+	void removeAt(size_t at) {
+		ListMember* ptr = list;
+		for (size_t i = 0; i < at; i++) {
+			ptr = ptr->next;
+		}
+		ptr->prev->next = ptr->next;
+		delete ptr;
+		length--;
+	}
+
+	Iterator begin() {
+		return Iterator(list);
+	}
+
+	Iterator end() {
+		return Iterator(NULL);
+	}
+
+	void operator+=(T element) {
+		ListMember* ptr = list;
+		if (ptr != NULL) {
+			while ((ptr->next != NULL)) {
+				if (!pred(ptr->next->element, element)) {
+					break;
+				}
+				ptr = ptr->next;
+			}
+		}
+		ListMember* newPtr = new ListMember(element, ptr, ptr ? ptr->next : NULL);
+		if (newPtr->prev == NULL) {
+			list = newPtr;
+		}
+		length++;
+	}
+
+	~SortedList() {
+		ListMember* ptr = list;
+		while (ptr != NULL) {
+			ListMember* oldPtr = ptr;
+			ptr = ptr->next;
+			delete oldPtr;
+		}
+	}
+};
+#endif // !SORTEDLIST
